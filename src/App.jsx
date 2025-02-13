5253cfeeb1214ff5f66dc7d5228bb08435eb22f4
mohammed <arifcsidhik@gmail.com>
2025-02-13 21:33:50 +0530

    react configration completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <p className="text-3xl font-bold text-cyan-600">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
